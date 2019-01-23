1988d591925deb75899d17ceb266ec72184f3b1c
JoGom <jogoram428@gmail.com>
2019-01-23 14:11:43 -0700

    fixed moment.js bug

--- liri.js
+++ liri.js
@@ -10,37 +10,37 @@ const input = process.argv;
 let command = input[2];
 let commandParam = input.slice(3).join(" ");;
 
-const eventInfo = function(){
-    let queryUrl = "https://rest.bandsintown.com/artists/"+commandParam+"/events?app_id=codingbootcamp"
+const eventInfo = function(param){
+    let queryUrl = "https://rest.bandsintown.com/artists/"+param+"/events?app_id=codingbootcamp"
     axios.get(queryUrl).then(
         function(response) {
             if(response.data.length === 0){
-                console.log(`\n*** No upcoming events for ${commandParam} ***\n`);
+                console.log(`\n*** No upcoming events for ${param} ***\n`);
             }
             else{
-            console.log(`\n*** Showing event results for ${commandParam} ***\n`);
+            console.log(`\n*** Showing event results for ${param} ***\n`);
             for(let i = 0; i < response.data.length; i++){
             console.log(`Name of Venue: ${response.data[i].venue.name}\n--`);
             console.log(`Venue Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}\n--`);
-            console.log(`Date of Event: ${moment(response.data[0].datetime).format('MM/DD/YYYY')}\n`);
+            console.log(`Date of Event: ${moment(response.data[i].datetime).format('MM/DD/YYYY')}\n`);
             console.log("========================");
             };
             };  
         });    
 };
 
-const spotifyInfo = function(){
+const spotifyInfo = function(param){
     spotify = new Spotify(keys.spotify);
-    if(!commandParam){
-        commandParam = "The Sign Ace of Base"
+    if(!param){
+        param = "The Sign Ace of Base"
     }
-    spotify.search({ type: 'track', query: commandParam, limit: 3 }, function(err, data) {
+    spotify.search({ type: 'track', query: param, limit: 3 }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        console.log(`\n*** Showing song results for ${param} ***\n`);
         for(let i = 0; i < data.tracks.items.length; i++){
             let x = data.tracks.items[i];
-            console.log(`\n*** Showing song results for ${commandParam} ***\n`);
             console.log(`Artist(s): ${x.artists[0].name}\n--`);
             console.log(`Song Name: ${x.name}\n--`);
             console.log(`Preview Song: ${x.preview_url}\n--`);
@@ -50,17 +50,17 @@ const spotifyInfo = function(){
       });
 }
 
-const movieInfo = function(){
-    let queryUrl = "http://www.omdbapi.com/?t=" + commandParam + "&y=&plot=short&apikey=trilogy";
+const movieInfo = function(param){
+    let queryUrl = "http://www.omdbapi.com/?t=" + param + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
         function(response) {
             let x = response.data;
-            if(!commandParam){
-                commandParam = "Mr. Nobody";
-                movieInfo();
+            if(!param){
+                param = "Mr. Nobody";
+                movieInfo(param);
             }
             else{
-            console.log(`\n*** Showing movie results for ${commandParam} ***\n`);
+            console.log(`\n*** Showing movie results for ${param} ***\n`);
             console.log(`Movie Title: ${x.Title}\n--`);
             console.log(`Year: ${x.Year}\n--`);
             console.log(`IMDB Rating: ${x.imdbRating}\n--`);
@@ -84,24 +84,23 @@ const readFile = function(){
         for(let i = 0; i < dataLines.length; i++){
             let dataArray = dataLines[i].split(",");
             command = dataArray[0];
+            //gets rid of the quotes around the string
             commandParam = dataArray[1].replace(/['"]+/g, '');
-            console.log(command);
-            console.log(commandParam);
-            logic();
+            logic(command);
         }
     });
 }
-
-const logic = function(){
+//switch statements that determine what function will be executed
+const logic = function(command){
     switch(command){
         case "concert-this":
-            eventInfo();
+            eventInfo(commandParam);
             break;
         case "spotify-this-song":
-            spotifyInfo();
+            spotifyInfo(commandParam);
             break;
         case "movie-this":
-            movieInfo();
+            movieInfo(commandParam);
             break;
         case "do-what-it-says":
             readFile();
@@ -110,4 +109,4 @@ const logic = function(){
             return undefined;
     };
 };
-logic();
+logic(command);
