6a19b94a257860e8425104cd99ba4827ea306cf1
JoGom <jogoram428@gmail.com>
2019-01-22 22:55:56 -0700

    bands in town api working

--- liri.js
+++ liri.js
@@ -10,50 +10,53 @@ const input = process.argv;
 const command = input[2];
     console.log(command);
     console.log(input);
-let commandParam = "";
+let commandParam = process.argv.slice(3).join(" ");;
 
-const parametersOmdb = function(){
-    for (let i = 3; i < input.length; i++) {
-        if (i > 3 && i < input.length) {
-          commandParam = commandParam + "+" + input[i];
-        }
-        else {
-          commandParam += input[i];   
-        }
-      }      
-}
-const parametersBands = function(){
-    for (let i = 3; i < input.length; i++) {
-        if (i > 3 && i < input.length) {
-          commandParam = commandParam + "%20" + input[i];
-        }
-        else {
-          commandParam += input[i];   
-        }
-      }      
-}
-const parametersNpm = function(){
-    for (let i = 3; i < input.length; i++) {
-        if (i > 3 && i < input.length) {
-          commandParam = commandParam + " " + input[i];
-        }
-        else {
-          commandParam += input[i];   
-        }
-      }      
-}  
+// const parametersOmdb = function(){
+//     for (let i = 3; i < input.length; i++) {
+//         if (i > 3 && i < input.length) {
+//           commandParam = commandParam + "+" + input[i];
+//         }
+//         else {
+//           commandParam += input[i];   
+//         }
+//       }      
+// }
+// const parametersBands = function(){
+//     for (let i = 3; i < input.length; i++) {
+//         if (i > 3 && i < input.length) {
+//           commandParam = commandParam + "%20" + input[i];
+//         }
+//         else {
+//           commandParam += input[i];   
+//         }
+//       }      
+// }
+// const parametersNpm = function(){
+//     for (let i = 3; i < input.length; i++) {
+//         if (i > 3 && i < input.length) {
+//           commandParam = commandParam + " " + input[i];
+//         }
+//         else {
+//           commandParam += input[i];   
+//         }
+//       }      
+//}  
 // parametersNpm();
 // console.log(commandParam);
 const eventInfo = function(){
-    parametersBands();
     let queryUrl = "https://rest.bandsintown.com/artists/"+commandParam+"/events?app_id=codingbootcamp"
     console.log(queryUrl);
 
     axios.get(queryUrl).then(
         function(response) {
+            console.log(`\n*** Showing event results for ${commandParam} ***\n`);
             for(let i = 0; i < response.data.length; i++){
-                console.log(`Name of Venue: ${response.data[i].venue.name}`);
-            }
+            console.log(`Name of Venue: ${response.data[i].venue.name}\n--`);
+            console.log(`Venue Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}\n--`);
+            console.log(`Date of Event: ${moment(response.data[0].datetime).format('MM/DD/YYYY')}`);
+            console.log("========================")
+            };
         });    
 };
 
